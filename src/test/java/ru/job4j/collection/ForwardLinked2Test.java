c1cbeed209d75727cb8b1edc01c8b1d7ebc27fb1
Alexander.Banar <s6M-HN2-EpL-9CX>
2021-01-05 22:20:16 +0300

    6. Перевернуть связанный список [#435830]

---
+++ src/test/java/ru/job4j/collection/ForwardLinked2Test.java
@@ -0,0 +1,31 @@
+package ru.job4j.collection;
+
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+import org.junit.Test;
+
+import java.util.Iterator;
+
+public class ForwardLinked2Test {
+    @Test
+    public void whenAddThenIter() {
+        ForwardLinked2<Integer> linked = new ForwardLinked2<>();
+        linked.add(1);
+        linked.add(2);
+        Iterator<Integer> it = linked.iterator();
+        assertThat(it.next(), is(1));
+        assertThat(it.next(), is(2));
+    }
+
+    @Test
+    public void whenAddAndRevertThenIter() {
+        ForwardLinked2<Integer> linked = new ForwardLinked2<>();
+        linked.add(1);
+        linked.add(2);
+        linked.revert();
+        Iterator<Integer> it = linked.iterator();
+        assertThat(it.next(), is(2));
+        assertThat(it.next(), is(1));
+    }
+}
