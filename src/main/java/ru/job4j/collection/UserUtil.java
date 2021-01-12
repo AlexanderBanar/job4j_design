2327422c21c59ee51297cf94c7155123c098629c
AlexanderBanar <s595659@mail.ru>
2021-01-12 19:53:45 +0300

    2. Без переопределения equals и hashCode [#435859]

---
+++ src/main/java/ru/job4j/collection/UserUtil.java
@@ -0,0 +1,14 @@
+package ru.job4j.collection;
+
+import java.util.GregorianCalendar;
+import java.util.HashMap;
+import java.util.Map;
+
+public class UserUtil {
+    public static void main(String[] args) {
+        User first = new User("Ivan", 2, new GregorianCalendar(1987, 2, 2));
+        User second = new User("Ivan", 2, new GregorianCalendar(1987, 2, 2));
+        Map<User, Object> tester = new HashMap<>(Map.of(first, "First", second, "Second"));
+        System.out.println(tester);
+    }
+}
