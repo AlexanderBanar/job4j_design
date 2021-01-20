8746a4c29a2fb08b94fedf3bd08c5be9e4fc383b
AlexanderBanar <s595659@mail.ru>
2021-01-20 21:28:28 +0300

    2. Статистику по коллекции. [#435820] _ refactored

--- src/main/java/ru/job4j/collection/Analyze.java
+++ src/main/java/ru/job4j/collection/Analyze.java
@@ -1,22 +1,33 @@
 package ru.job4j.collection;
 
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class Analyze {
     public Info diff(List<User> previous, List<User> current) {
         Info rsl = new Info(0, 0, 0);
-        for (User prev : previous) {
-            for (int i = 0; i < current.size(); i++) {
-                User curr = current.get(i);
-                if (prev.id == curr.id) {
-                    if (!prev.name.equals(curr.name)) {
-                        rsl.changed++;
-                    }
-                    break;
+        Map<Integer, User> prev = previous.stream()
+                .collect(Collectors.toMap(
+                        x -> x.id,
+                        x -> x
+                ));
+        Map<Integer, User> curr = current.stream()
+                .collect(Collectors.toMap(
+                        x -> x.id,
+                        x -> x
+                ));
+        Set<Integer> prevSet = prev.keySet();
+        for (Integer temp : prevSet) {
+            if (curr.containsKey(temp)) {
+                User p = prev.get(temp);
+                User c = curr.get(temp);
+                if (!p.name.equals(c.name)) {
+                    rsl.changed++;
                 }
-                if (i == current.size() - 1) {
-                    rsl.deleted++;
-                }
+            } else {
+                rsl.deleted++;
             }
         }
         rsl.added = current.size() - previous.size() + rsl.deleted;
