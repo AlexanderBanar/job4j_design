84c20619849b3e9355287fd70776a1b168c4e644
AlexanderBanar <s595659@mail.ru>
2021-01-24 21:42:52 +0300

    Почта. [#184458]

---
+++ src/main/java/ru/job4j/collection/Email.java
@@ -0,0 +1,91 @@
+package ru.job4j.collection;
+
+import java.util.*;
+
+/**
+ * Создал мапу для того, чтобы в нее закидывать все эл. адреса как ключи со значением индекса юзера из входящего листа
+ *
+ * лист removals хранит индексы всех юзеров, которые были соединены с исходным юзером - для последующего удаления
+ *
+ * перем. toMerge становится true, когда выпадает истина из проверки содержит ли мапа уже данный адрес
+ * далее по циклу toMerge перекидывает все адреса исходного и дублирующего пользователя в Сет, чтобы удалить дубли
+ * и сливает в один список данные адреса, затем создает пользователя и переписывает им исходного, также заносит
+ * в лист removals индекс юзера, который далее удаляется из возвращаемого списка в методе
+ *
+ * перем. reDo начинает заново цикл уже со "знанием", что исходный пользователь уже имеется и нужно адреса
+ * текущего закидывать в мапу с индексом исходного, а не текущего
+ */
+
+public class Email {
+    public List<User> compress(List<User> initial) {
+        Map<String, Integer> map = new HashMap<>();
+        List<Integer> removals = new ArrayList<>();
+        boolean toMerge = false;
+        boolean reDo = false;
+        int index = 0;
+        for (int i = 0; i < initial.size(); i++) {
+            List<String> tempEmails = initial.get(i).addresses;
+            for (int k = 0; k < tempEmails.size(); k++) {
+                String n = tempEmails.get(k);
+                if (!reDo) {
+                    if (map.containsKey(n)) {
+                        toMerge = true;
+                        index = map.get(n);
+                        k = 0;
+                        reDo = true;
+                        continue;
+                    } else {
+                        map.put(n, i);
+                    }
+                } else {
+                    map.put(n, index);
+                }
+            }
+            reDo = false;
+            if (toMerge) {
+                User existing = initial.get(index);
+                Set<String> modifiedSet = new HashSet<>();
+                modifiedSet.addAll(existing.addresses);
+                modifiedSet.addAll(tempEmails);
+                List<String> modifiedList = new ArrayList<>(modifiedSet);
+                initial.set(index, new User(existing.name, modifiedList));
+                removals.add(i);
+                toMerge = false;
+            }
+        }
+        int r = 0;
+        for (Integer i : removals) {
+            initial.remove((r + i));
+            r--;
+        }
+        return initial;
+    }
+
+    public static class User {
+        String name;
+        List<String> addresses;
+
+        public User(String name, List<String> addresses) {
+            this.name = name;
+            this.addresses = addresses;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) {
+                return true;
+            }
+            if (o == null || getClass() != o.getClass()) {
+                return false;
+            }
+            ru.job4j.collection.Email.User user = (ru.job4j.collection.Email.User) o;
+            return Objects.equals(name, user.name)
+                    && Objects.equals(addresses, user.addresses);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(name, addresses);
+        }
+    }
+}
---
+++ src/test/java/ru/job4j/collection/EmailTest.java
@@ -0,0 +1,99 @@
+package ru.job4j.collection;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+public class EmailTest {
+    @Test
+    public void whenNoChanges() {
+        Email.User user1 = new Email.User("user1", new ArrayList<>(List.of("a", "b", "c")));
+        Email.User user2 = new Email.User("user2", new ArrayList<>(List.of("d", "e")));
+        List<Email.User> list = new ArrayList<>();
+        list.add(user1);
+        list.add(user2);
+        List<Email.User> rsl = new Email().compress(list);
+        assertThat(rsl, is(list));
+    }
+
+    @Test
+    public void when1UserHasSameMails() {
+        Email.User user1 = new Email.User("user1", new ArrayList<>(List.of("a", "b", "c")));
+        Email.User user2 = new Email.User("user2", new ArrayList<>(List.of("d", "e")));
+        Email.User user3 = new Email.User("user3", new ArrayList<>(List.of("c", "n")));
+        Email.User user4 = new Email.User("user1", new ArrayList<>(List.of("a", "b", "c", "n")));
+        List<Email.User> list = new ArrayList<>();
+        list.add(user1);
+        list.add(user2);
+        list.add(user3);
+        List<Email.User> listCompressed = new ArrayList<>();
+        listCompressed.add(user4);
+        listCompressed.add(user2);
+        List<Email.User> rsl = new Email().compress(list);
+        assertThat(rsl, is(listCompressed));
+    }
+
+    @Test
+    public void when2UserHasSameMailsAndDuplicatesMails() {
+        Email.User user1 = new Email.User("user1", new ArrayList<>(List.of("a", "b", "c")));
+        Email.User user2 = new Email.User("user2", new ArrayList<>(List.of("d", "e")));
+        Email.User user3 = new Email.User("user3", new ArrayList<>(List.of("c")));
+        Email.User user4 = new Email.User("user4", new ArrayList<>(List.of("k", "n", "e", "j", "e")));
+        Email.User user5 = new Email.User("user5", new ArrayList<>(List.of("w", "o")));
+        Email.User user7 = new Email.User("user2", new ArrayList<>(List.of("d", "e", "j", "k", "n")));
+        List<Email.User> list = new ArrayList<>();
+        list.add(user1);
+        list.add(user2);
+        list.add(user3);
+        list.add(user4);
+        list.add(user5);
+        List<Email.User> rsl = new Email().compress(list);
+        assertThat(rsl.contains(user1), is(true));
+        assertThat(rsl.contains(user7), is(true));
+        assertThat(rsl.contains(user2), is(false));
+        assertThat(rsl.contains(user5), is(true));
+        assertThat(rsl.size(), is(3));
+    }
+
+    @Test
+    public void whenAllUsersAreOne() {
+        Email.User user1 = new Email.User("user1", new ArrayList<>(List.of("a", "b")));
+        Email.User user2 = new Email.User("user2", new ArrayList<>(List.of("b", "c")));
+        Email.User user3 = new Email.User("user3", new ArrayList<>(List.of("c", "d")));
+        Email.User user4 = new Email.User("user4", new ArrayList<>(List.of("d")));
+        Email.User user5 = new Email.User("user1", new ArrayList<>(List.of("a", "b", "c", "d")));
+        List<Email.User> list = new ArrayList<>();
+        list.add(user1);
+        list.add(user2);
+        list.add(user3);
+        list.add(user4);
+        List<Email.User> rsl = new Email().compress(list);
+        assertThat(rsl.contains(user5), is(true));
+        assertThat(rsl.contains(user2), is(false));
+        assertThat(rsl.size(), is(1));
+    }
+
+    @Test
+    public void whenFirstUserIsMainAndOthersToDel() {
+        Email.User user1 = new Email.User("user1", new ArrayList<>(List.of("a")));
+        Email.User user2 = new Email.User("user2", new ArrayList<>(List.of("a", "b", "c", "d")));
+        Email.User user3 = new Email.User("user3", new ArrayList<>(List.of("b")));
+        Email.User user4 = new Email.User("user4", new ArrayList<>(List.of("c")));
+        Email.User user5 = new Email.User("user1", new ArrayList<>(List.of("d")));
+        Email.User userA = new Email.User("user1", new ArrayList<>(List.of("a", "b", "c", "d")));
+        List<Email.User> list = new ArrayList<>();
+        list.add(user1);
+        list.add(user2);
+        list.add(user3);
+        list.add(user4);
+        list.add(user5);
+        List<Email.User> rsl = new Email().compress(list);
+        assertThat(rsl.contains(userA), is(true));
+        assertThat(rsl.contains(user3), is(false));
+        assertThat(rsl.size(), is(1));
+    }
+}
