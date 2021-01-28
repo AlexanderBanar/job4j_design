a566fc61c720bc90d0f14137f32fe6425a7e7597
Alexander.Banar <s6M-HN2-EpL-9CX>
2021-01-28 23:15:00 +0300

    Maven test error fix for Travis build

--- src/main/java/ru/job4j/io/ResultFile.java
+++ src/main/java/ru/job4j/io/ResultFile.java
@@ -6,7 +6,7 @@ public class ResultFile {
     public static void main(String[] args) {
         StringBuilder s = new StringBuilder();
         for (int x = 0; x <= 9; x++) {
-            for (int i = 9; i >= 0 ; i--) {
+            for (int i = 9; i >= 0; i--) {
                 int rsl = x * i;
                 s.append(x).append(" * ").append(i).append(" = ")
                         .append(rsl).append(System.lineSeparator());
