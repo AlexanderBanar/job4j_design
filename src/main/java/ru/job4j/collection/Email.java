60667625edf5603f9c21f6e46aec0e4f28e741b9
Alexander.Banar <s6M-HN2-EpL-9CX>
2021-01-28 18:26:05 +0300

    Return to prev ver (Почта)

--- src/main/java/ru/job4j/collection/Email.java
+++ src/main/java/ru/job4j/collection/Email.java
@@ -2,20 +2,6 @@ package ru.job4j.collection;
 
 import java.util.*;
 
-/**
- * Создал мапу для того, чтобы в нее закидывать все эл. адреса как ключи со значением индекса юзера из входящего листа
- *
- * лист removals хранит индексы всех юзеров, которые были соединены с исходным юзером - для последующего удаления
- *
- * перем. toMerge становится true, когда выпадает истина из проверки содержит ли мапа уже данный адрес
- * далее по циклу toMerge перекидывает все адреса исходного и дублирующего пользователя в Сет, чтобы удалить дубли
- * и сливает в один список данные адреса, затем создает пользователя и переписывает им исходного, также заносит
- * в лист removals индекс юзера, который далее удаляется из возвращаемого списка в методе
- *
- * перем. reDo начинает заново цикл уже со "знанием", что исходный пользователь уже имеется и нужно адреса
- * текущего закидывать в мапу с индексом исходного, а не текущего
- */
-
 public class Email {
     public List<User> compress(List<User> initial) {
         Map<String, Integer> map = new HashMap<>();
@@ -59,32 +45,8 @@ public class Email {
             r--;
         }
         return initial;
-
-//        Map<String, User> map = new HashMap<>();
-//
-//        for (int i = 0; i < initial.size(); i++) {
-//            User user = initial.get(i);
-//            List<String> tempEmails = user.addresses;
-//
-//            for (int k = 0; k < tempEmails.size(); k++) {
-//                String n = tempEmails.get(k);
-//
-//                if (!map.containsKey(n)) {
-//                    map.put(n, user);
-//                } else {
-//                    user = map.get(n);
-//                    k = 0;
-//                    continue;
-//                }
-//
-//
-//
-//            }
-//
-//
-//        }
-
     }
+
 
     public static class User {
         String name;
