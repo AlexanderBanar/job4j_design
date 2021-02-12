f7e37cc46a26e5172fe3346a4a6f6050f74456b8
AlexanderBanar <s595659@mail.ru>
2021-02-12 20:55:37 +0300

    4.2. Поиск дубликатов [#435880] _ refactored

--- src/main/java/ru/job4j/io/duplicates/DuplicatesFinder.java
+++ src/main/java/ru/job4j/io/duplicates/DuplicatesFinder.java
@@ -6,6 +6,9 @@ import java.nio.file.Path;
 
 public class DuplicatesFinder {
     public static void main(String[] args) throws IOException {
-        Files.walkFileTree(Path.of("./"), new DuplicatesVisitor());
+        if (args[0] == null || args[0].isEmpty()) {
+            throw new IllegalArgumentException("Directory is not specified!");
+        }
+        Files.walkFileTree(Path.of(args[0]), new DuplicatesVisitor());
     }
 }
--- src/main/java/ru/job4j/io/duplicates/DuplicatesVisitor.java
+++ src/main/java/ru/job4j/io/duplicates/DuplicatesVisitor.java
@@ -5,32 +5,20 @@ import java.nio.file.FileVisitResult;
 import java.nio.file.Path;
 import java.nio.file.SimpleFileVisitor;
 import java.nio.file.attribute.BasicFileAttributes;
-import java.util.Map;
+import java.util.HashSet;
+import java.util.Set;
 
 public class DuplicatesVisitor extends SimpleFileVisitor<Path> {
-    private Map<String, FileProperty> registry;
-    private Path duplicated;
+    private Set<FileProperty> registry = new HashSet<>();
 
     @Override
     public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
-        System.out.println(file.toAbsolutePath());
-        if (isDuplicate(file)) {
-            System.out.print(" The file is duplicate to: ");
-            System.out.print(duplicated.toString());
-        }
-        return super.visitFile(file, attrs);
-    }
-
-    private boolean isDuplicate(Path file) {
         String fileName = file.getFileName().toString();
         FileProperty currentFile = new FileProperty(file.toFile().length(), fileName);
-        if (registry.containsKey(fileName)) {
-            if (currentFile.equals(registry.get(fileName))) {
-                duplicated = file;
-                return true;
-            }
+        if (!registry.add(currentFile)) {
+            System.out.print(fileName);
+            System.out.println(" is duplicate");
         }
-        registry.put(fileName, currentFile);
-        return false;
+        return super.visitFile(file, attrs);
     }
 }
