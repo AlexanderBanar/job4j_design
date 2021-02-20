d00f280c706c94e22da9927d9577a4bb133a30e8
AlexanderBanar <s595659@mail.ru>
2021-02-20 20:53:16 +0300

    3. Slf4j - вывод переменных. [#435869]

--- src/main/java/ru/job4j/io/UsageLog4j.java
+++ src/main/java/ru/job4j/io/UsageLog4j.java
@@ -7,10 +7,23 @@ public class UsageLog4j {
     private static final Logger LOG = LoggerFactory.getLogger(UsageLog4j.class.getName());
 
     public static void main(String[] args) {
-        LOG.trace("trace message");
-        LOG.debug("debug message");
-        LOG.info("info message");
-        LOG.warn("warn message");
-        LOG.error("error message");
+        String name = "Alex Banar";
+        int age = 33;
+        char grade = 'A';
+        boolean activity = true;
+        long account = 686678737756871L;
+        float completion = 0.4F;
+        double height = 1.78;
+        byte course = 2;
+        short days = 212;
+
+
+ //       LOG.trace("trace message");
+        LOG.debug("User info name : {}, age : {}, grade : {}, activity : {}," +
+                " account : {}, completion : {}, height : {}, course : {}, days : {}",
+                name, age, grade, activity, account, completion, height, course, days);
+ //       LOG.info("info message");
+ //       LOG.warn("warn message");
+ //       LOG.error("error message");
     }
 }
