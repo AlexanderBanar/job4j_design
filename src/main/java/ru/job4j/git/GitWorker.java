67dd0f44662cffecde6d05a2b59f6e2e8f91537e
AlexanderBanar <s595659@mail.ru>
2021-07-16 07:45:48 +0300

    3.2. Откат изменений. [#307144] _ step 4

--- src/main/java/ru/job4j/git/GitWorker.java
+++ src/main/java/ru/job4j/git/GitWorker.java
@@ -3,5 +3,8 @@ package ru.job4j.git;
 public class GitWorker {
     public static void main(String[] args) {
         System.out.println("This is first commit in the commit chain");
+        System.out.println("This is second commit in the commit chain");
+        System.out.println("This is third commit in the commit chain");
+        System.out.println("____");
     }
 }
